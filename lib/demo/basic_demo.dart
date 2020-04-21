8378f7e99a83d24f05b97532c2f42576efef1d2c
automation2015 <maqinmaqin@126.com>
2020-04-22 07:57:03 +0800

    basic202022

---
+++ lib/demo/basic_demo.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+class BasicDemo extends StatelessWidget{
+ 
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        image:DecorationImage(
+          image:NetworkImage('https://resources.ninghao.org/images/say-hello-to-barry.jpg'), 
+          alignment: Alignment.topCenter,
+         // repeat: ImageRepeat.repeat,
+         fit:BoxFit.cover,
+         colorFilter: ColorFilter.mode(
+           Colors.indigoAccent[400].withOpacity(0.5),
+           BlendMode.hardLight,
+         )
+          )
+           ),
+      //color: Colors.grey[100],
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            
+            child: Icon(Icons.pool,size:32.0,color: Colors.white,),
+            //color: Color.fromRGBO(3,54,255,1.0),
+            padding: EdgeInsets.all(16.0),
+            margin: EdgeInsets.all(8.0),
+            width: 90.0,
+            height: 90.0,
+            decoration: BoxDecoration(
+              color: Color.fromRGBO(3,54,255,1.0),
+              border: Border.all(
+                color: Colors.indigoAccent[100],width:3.0,style: BorderStyle.solid),
+              //borderRadius: BorderRadius.only(topLeft: Radius.circular(64.0),bottomLeft:Radius.circular(64.0)),
+              boxShadow: [
+                BoxShadow(
+                  offset: Offset(6.0,7.0),//偏移量
+                  color:Color.fromRGBO(16, 20, 188, 1.0),//阴影默认颜色是黑色
+                  blurRadius: 1.0,//阴影的模糊程度，值越大，越模糊
+                  spreadRadius: -3.0,//阴影的面积，正值扩大，负值缩小
+                  
+                  )
+              ],
+              shape: BoxShape.circle,//使用圆形时不能同时使用borderRadius设置
+              //线性渐变：默认开始位置是左->右，可以设置开始和结束位置
+              gradient: LinearGradient(
+                colors: [
+                  Color.fromRGBO(7,102, 255, 1.0),
+                  Color.fromRGBO(3,28, 128, 1.0),
+                ],
+                begin:Alignment.topCenter,
+                end:Alignment.bottomCenter
+                )
+                /*
+                gradient: RadialGradient(
+                colors: [
+                  Color.fromRGBO(7,102, 255, 1.0),
+                  Color.fromRGBO(3,28, 128, 1.0),
+                ])
+                */
+              )
+             
+              //borderRadius: BorderRadius.circular(16.0))
+              /*
+              border: Border(
+                top:BorderSide(
+                  color: Colors.indigoAccent[100],width:3.0,style: BorderStyle.solid),
+                bottom: BorderSide(
+                  color: Colors.indigoAccent[100],width:3.0,style: BorderStyle.solid))),
+                
+                  */
+          )],
+
+      ),);
+  }
+}
+class RichTextDemo extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+      text:TextSpan(
+        text:"hello richText",
+        style: TextStyle(
+          color: Colors.red,
+          fontSize:34.0,
+          fontStyle: FontStyle.italic,
+          fontWeight: FontWeight.w200
+           ),
+        children: [
+          TextSpan(
+          text: '.net',
+          style: TextStyle(
+            fontSize: 17.0,
+            fontStyle: FontStyle.italic,
+            color: Colors.purple) )
+
+        ])
+      );;
+  }
+}
+class TextDemo extends StatelessWidget{
+   final TextStyle _textStyle=TextStyle(fontSize: 16.0);
+  final String _author='李白';
+final String _title='将进酒';
+  @override
+  Widget build(BuildContext context) {
+    return Text('《$_title》--$_author。君不见黄河之水天上来，奔流到海不复回。君不见高堂明镜悲白发，朝如青丝暮成雪。人生得意须尽欢，莫使金樽空对月。天生我材必有用，千金散尽还复来。烹羊宰牛且为乐，会须一饮三百杯。'
+    ,textAlign: TextAlign.left,
+    style: _textStyle,
+    maxLines: 3,
+    overflow: TextOverflow.ellipsis,
+    );
+  }
+}
